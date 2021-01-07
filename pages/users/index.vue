1ac3e5ea7692b97006941cf1bbca0213121025b9
arief0303 <arief0303@gmail.com>
2021-01-07 18:35:22 +0100

    meta update

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,24 +1,31 @@
 <template>
-<div>
-<h1>Hello users</h1>
-<h6>Regex only accepts numerals, no letters allowed.</h6>
-<input type="text" v-model="id">
-<button @click="loadUser">Load user</button>
-</div>
+  <div>
+    <h1>Nested users page demo</h1>
+    <h6>Regex only accepts numerals, no letters allowed.</h6>
+    <input type="text" v-model="id" />
+    <button @click="loadUser">Load user</button>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            id: ''
-        }
-    },
+  data() {
+    return {
+      id: "",
+      title: "Nested pages demo",
+    };
+  },
 
-    methods: {
-        loadUser() {
-            this.$router.push(`/users/${this.id}`)
-        }
-    }
-}
+  head() {
+    return {
+      title: this.title,
+    };
+  },
+
+  methods: {
+    loadUser() {
+      this.$router.push(`/users/${this.id}`);
+    },
+  },
+};
 </script>
